2121df83accbe6965e384557c8fae4e830b2018f
JASON MCNAIR <jmcn2000@yahoo.com>
2022-02-17 21:26:12 -0500

    Initial Push

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using ConsumeSpotifyAPIs.Models;
+using ConsumeSpotifyWebAPIs.Models;
+using ConsumeSpotifyWebAPIs.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumeSpotifyWebAPIs.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ISpotifyAccountService _spotifyAccountService;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ISpotifyAccountService spotifyAccountService, IConfiguration configuration)
+        {
+            _spotifyAccountService = spotifyAccountService;
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var token = await _spotifyAccountService.GetToken(_configuration["Spotify:ClientId"], _configuration["Spotify:ClientSecret"]);
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Services/SpotifyAccountService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using ConsumeSpotifyWebAPIs.Models;
+using System.Net.Http.Headers;
+
+namespace ConsumeSpotifyWebAPIs.Services
+{
+    public class SpotifyAccountService : ISpotifyAccountService
+    {
+        private readonly HttpClient _httpClient;
+        public SpotifyAccountService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetToken(string clientId, string clientSecret)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "token");
+
+            request.Headers.Authorization = new AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}")));
+
+            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                {"grant_type","client_credentials" }
+            });
+
+            var response = await _httpClient.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            var authResult = await JsonSerializer.DeserializeAsync<AuthResult>(responseStream);
+
+            return authResult.access_token;
+        }
+    }
+}
